fae0406d4b3f153291b9716c3103823aede8d338
Sajal Gupta <21bei051@ietdavv.edu.in>
2024-11-17 22:50:29 +0530

    URL Fix

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== "production") {
+
 	app.use(
 		cors({
 			origin: process.env.CLIENT_URL,
 			credentials: true,
 		})
 	);
-}
+
 
 app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
 app.use(cookieParser());
